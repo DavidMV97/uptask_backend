ea855d24157aee6efbd35dbf9043c80114ecda69
Germán David <manchov12@gmail.com>
2024-07-07 13:58:59 -0500

    creating middleware - creating enpoints crud

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,44 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body, param } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La description del Proyecto es Obligatoria'),
+    handleInputErrors,
+    ProjectController.createProject
+)
+
 router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id',
+    param('id').isMongoId().withMessage('ID no válido'),
+    handleInputErrors,
+    ProjectController.getProjectById
+)
+
+router.put('/:id',
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La description del Proyecto es Obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject
+)
+
+router.delete('/:id',
+    param('id').isMongoId().withMessage('ID no válido'),
+    handleInputErrors,
+    ProjectController.deleteProject
+)
 
 export default router
