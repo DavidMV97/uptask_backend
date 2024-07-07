9600fc8d502fce709103f9a723c51da79a8a8ddd
Germán David <manchov12@gmail.com>
2024-07-07 16:05:23 -0500

    Creating model and Task Controller

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
 import { body, param } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
+import { TaskController } from "../controllers/TaskController";
 
 const router = Router()
 
@@ -39,6 +40,11 @@ router.delete('/:id',
     param('id').isMongoId().withMessage('ID no válido'),
     handleInputErrors,
     ProjectController.deleteProject
+)
+
+/** Routes for tasks **/
+router.post('/:projectId/tasks',
+    TaskController.createTask
 )
 
 export default router
