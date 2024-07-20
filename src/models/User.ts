de8d7618e8c6fbe26afcb611295f3df6558a3beb
Germán David <manchov12@gmail.com>
2024-07-19 20:08:30 -0500

    working model user

---
+++ src/models/User.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+    email: string
+    password: string
+    name: string
+    confirmed: boolean
+}
+
+const userSchema: Schema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: {
+            type: String,
+            required: true
+        }
+    },
+    name: {
+        type: {
+            type: String,
+            required: true
+        }
+    },
+    confirmed: {
+        type: {
+            type: Boolean,
+            default: false
+        }
+    }
+})
+
+const User = mongoose.model<IUser>('user', userSchema)
+export default User
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { connectDB } from './config/db'
-import projectRoutes from './routes/projectRoutes'
-import { corsConfig } from './config/cors'
 import morgan from 'morgan'
+import { connectDB } from './config/db'
+import { corsConfig } from './config/cors'
+import projectRoutes from './routes/projectRoutes'
+import authRoutes from './routes/authRoutes'
 
 
 dotenv.config()
@@ -17,6 +18,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 // Routes
+app.use('/api/auth', authRoutes)
 app.use('/api/projects',  projectRoutes )
 
 
