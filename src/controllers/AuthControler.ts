1ef6d9141389d5fba85c544327779be1f7dcfe2c
Germán David <manchov12@gmail.com>
2024-08-05 15:07:06 -0500

    enpoint request confirmation code

--- src/controllers/AuthControler.ts
+++ src/controllers/AuthControler.ts
@@ -99,4 +99,39 @@ export class AuthController {
             res.status(500).json({ error: 'Hubo un error' })
         }
     }
+
+    static requestConfirmationCode = async (req: Request, res: Response) => {
+        try {
+            const { email } = req.body
+            // Usuario existe
+            const user = await User.findOne({ email })
+            if (!user) {
+                const error = new Error('El Usuario no está registrado')
+                return res.status(404).json({ error: error.message })
+            }
+
+            if (user.confirmed) {
+                const error = new Error('El Usuario ya está confirmado')
+                return res.status(403).json({ error: error.message })
+            }
+
+            // Generar el token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+
+            // Enviar email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.name,
+                token: token.token
+            })
+
+            await Promise.allSettled([user.save(), token.save()])
+            res.send('Se envió un nuevo token a tu email')
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error creando el usuario' })
+        }
+    }
+
 }
